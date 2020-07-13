c4ad73c11bc1d74214eb26381d1bd8572eb897c8
Sarodace <jgferreira@wpi.edu>
2020-07-13 12:52:23 -0700

    Christmas lights implemented - LED lights now resemble christmas lights with vivid colors, such as red, yellow, cyan, etc..., spaced out appropriately!

--- LED.py
+++ LED.py
@@ -2,10 +2,29 @@ import board
 import neopixel
 from time import sleep
 
-pixels = neopixel.NeoPixel(board.D21, 26, brightness = 0.1)
-pixels.fill((0, 0, 0))
-sleep (2)
-pixels.fill((255, 255, 255))
-pixels.show()
+BLANK = (0, 0, 0)
+RED = (0, 255, 0)
+YELLOW = (150, 255, 0)
+CYAN = (255, 0, 255)
+GREEN = (255, 0, 0)
+BLUE = (0, 0, 255)
+PURPLE = (0, 180, 255)
 
-print("Hello, world")
+pixels = neopixel.NeoPixel(board.D21, 26, brightness = 0.1, auto_write=True)
+pixels.fill(BLANK)
+sleep (0.5)
+
+for i in range(len(pixels)):
+	sleep (0.1)
+	if i in range(0,26)[0::7]:
+		pixels[i] = RED
+	if i in range(0,26)[1::7]:
+		pixels[i] = YELLOW
+	if i in range(0,26)[2::7]:
+		pixels[i] = CYAN
+	if i in range(0,26)[3::7]:
+		pixels[i] = GREEN
+	if i in range(0,26)[4::7]:
+		pixels[i] = BLUE
+	if i in range(0,26)[5::7]:
+		pixels[i] = PURPLE
