68e97e8cad48a4f98e48d2316f2d3d0845992c0c
Sarodace <jgferreira@wpi.edu>
2020-07-13 13:00:19 -0700

    Commenting code

--- LED.py
+++ LED.py
@@ -1,7 +1,9 @@
+# LIBRARIES
 import board
 import neopixel
 from time import sleep
 
+# Potential LED colors
 BLANK = (0, 0, 0)
 RED = (0, 255, 0)
 YELLOW = (150, 255, 0)
@@ -10,10 +12,14 @@ GREEN = (255, 0, 0)
 BLUE = (0, 0, 255)
 PURPLE = (0, 180, 255)
 
+# Define LEDs that will be used
 pixels = neopixel.NeoPixel(board.D21, 26, brightness = 0.1, auto_write=True)
+
+# Clear any LED colors
 pixels.fill(BLANK)
 sleep (0.5)
 
+# Fill LEDs with appropriate colors
 for i in range(len(pixels)):
 	sleep (0.1)
 	if i in range(0,26)[0::7]:
@@ -28,3 +34,7 @@ for i in range(len(pixels)):
 		pixels[i] = BLUE
 	if i in range(0,26)[5::7]:
 		pixels[i] = PURPLE
+
+# TODO: MESSAGE DECOMP
+# TODO: RECEIVE SMS MESSAGE USING TWILIO OR SOME KIND OF IRC
+# TODO: CONVEY MESSAGE USING LEDs
