20a52da7a77608b0fefe1061ff4ad7b96b996667
Sarodace <jgferreira@wpi.edu>
2020-07-16 00:54:54 -0700

    Method to send message to the LED board! - Twilio or MQTT? Never heard of 'em! Discord bots are the way to go!   - Weird way to control a IoT project, but it strangely works pretty well! - Gotta work on integrating the two python files tomorrow/when I wake up

---
+++ bot.py
@@ -0,0 +1,46 @@
+# LIBRARIES
+import discord
+import os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+"""
+Eventually turn this into something like "@client.event" so that I just have a 
+dashboard that gets updated every so often with info like "messages sent", "uptime",
+and other relevant stuff.
+"""
+@client.command()
+async def embedt(ctx):
+
+    embed = discord.Embed(
+        colour=discord.Colour.blue(),
+        title = "Dashboard",
+        description="This is a test"
+    )
+
+    embed.add_field(name="Messages Sent:", value="Yoyo has received and seen xx messages", inline=False)
+    embed.set_footer(text="Footer")
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def send_message(ctx, arg):
+    # Check to see if message is alphanumeric, if not then return it and point out
+    # what characters aren't allowed. Maybe even try to auto-recommend something?
+
+    # Incorporate some kinda delay to ensure that I don't mispell anything
+
+    # Incorporate an embed, so that I could see info like "expected message display
+    # length", "message seen yet?"
+
+    # Support for "emotions", flash the LEDs in a certain way to make the message
+    # more meaningful
+    await ctx.send(arg)
+    print(arg)
+
+client.run(os.environ['ST_LED_DISCORD_BOT_KEY'])
